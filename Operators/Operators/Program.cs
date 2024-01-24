add14ae96f3e2e9c910c74de6a08c7104af15760
Coimbra <adsjkbd@gmail.com>
2024-01-24 19:25:05 -0300

    Operators

---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var a = 10;
+        var b = 3;
+        var c = 5;
+
+        Console.WriteLine(a / b); // 3
+        Console.WriteLine((float)a / (float)b); // 3,333333
+        Console.WriteLine(!(a != b));
+        Console.WriteLine(c > b && c > a);
+        Console.WriteLine(a < b || a == c);
+        Console.WriteLine(!(b < c || c == a));
+    }
+}
