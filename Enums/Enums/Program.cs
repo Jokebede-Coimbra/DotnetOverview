0b6835dceacdbdea3ec0561505b569727f7864d5
Coimbra <adsjkbd@gmail.com>
2024-01-25 13:52:32 -0300

    Demo: Enum

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,25 @@
+﻿public enum ShippingMethod
+{
+    RegularAirMail = 1,
+    RegisterdAirMail = 2,
+    Express = 3
+}
+internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+
+        var method = ShippingMethod.Express;
+        Console.WriteLine((int)method);
+
+        var methodId = 1;
+        Console.WriteLine((ShippingMethod)methodId);
+
+        Console.WriteLine(method.ToString());
+
+        var methodName = "Express";
+        var shippingMethod = (ShippingMethod) Enum.Parse(typeof(ShippingMethod), methodName);
+        Console.WriteLine(shippingMethod);
+    }
+}
