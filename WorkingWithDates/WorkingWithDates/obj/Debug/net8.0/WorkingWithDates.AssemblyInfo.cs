21ea2a85f7eac25e8450e2495b909421fd20eac9
Coimbra <adsjkbd@gmail.com>
2024-01-26 10:38:03 -0300

    Demo: String/StringBuilder

--- WorkingWithDates/WorkingWithDates/obj/Debug/net8.0/WorkingWithDates.AssemblyInfo.cs
+++ WorkingWithDates/WorkingWithDates/obj/Debug/net8.0/WorkingWithDates.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkingWithDates")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da6b98e21cf5bde81a0993ef3e15ad9c5411e838")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7a178ac0c9354879f3ba2338d526252c4b2ed55")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkingWithDates")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkingWithDates")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ WorkingWithText/WorkingWithText/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var fullName = "Coimbra Nascimento ";
+        Console.WriteLine("Trim: '{0}'", fullName.Trim());
+        Console.WriteLine("Trim: '{0}'", fullName.Trim().ToUpper());
+
+        var index = fullName.IndexOf(' ');
+        var firstName = fullName.Substring(0, index);
+        var lastName = fullName.Substring(index + 1);
+        Console.WriteLine("FirstName: " + firstName);
+        Console.WriteLine("LastName: " + lastName);
+
+        var names = fullName.Split(' ');
+        Console.WriteLine("FirstName: " + names[0]);
+        Console.WriteLine("LastName: " + names[1]);
+
+
+        Console.WriteLine(fullName.Replace("Coimbra", "Jkbd"));
+
+        if (String.IsNullOrWhiteSpace(" "))
+            Console.WriteLine("Invalid");
+
+        var str = "25";
+        var age = Convert.ToByte(str);
+        Console.WriteLine(age);
+
+        float price = 29.95f;
+        Console.WriteLine(price.ToString("C0"));
+
+
+        //*************************StringBuilder*****************************
+        var builder = new StringBuilder();
+        builder
+            .Append('-', 10)
+            .AppendLine()
+            .Append("Header")
+            .AppendLine()
+            .Append('-', 10);
+
+        builder.Replace('-', '*');
+
+        builder.Remove(0, 10);
+
+        builder.Insert(0, new string('-', 10));
+        Console.WriteLine(builder);
+    }
+}
