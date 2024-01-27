e212e92e255f486749abc52175b5315382b29c4e
Coimbra <adsjkbd@gmail.com>
2024-01-27 17:52:16 -0300

    Membros estáticos

--- Exercícios/ExercicioCurso/ExercicioNota/obj/Release/net8.0/ExercicioNota.AssemblyInfo.cs
+++ Exercícios/ExercicioCurso/ExercicioNota/obj/Release/net8.0/ExercicioNota.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExercicioNota")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5aa25885513fb2c0db47959ec09e509c45f2582f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExercicioNota")]
---
+++ MembrosEstáticos/MembrosEstáticos/Program.cs
@@ -0,0 +1,24 @@
+﻿using MembrosEstáticos;
+using System.Globalization;
+
+internal class Program
+{
+
+   
+    private static void Main(string[] args)
+    {
+        // Calculadora calculadora = new Calculadora();
+
+        Console.WriteLine("Entre  com o valor do raio: ");
+        double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double circ = Calculadora.Circunferencia(raio);
+        double vol = Calculadora.Volume(raio);
+
+        Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine("Volume: " + vol.ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+    }
+
+  
+}
