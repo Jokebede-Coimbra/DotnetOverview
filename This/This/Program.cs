dacc37a526fd96cf4c0e032f081e989adf16cc92
Coimbra <adsjkbd@gmail.com>
2024-01-30 05:24:33 -0300

    This

---
+++ This/This/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+using This;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Entre os dados do produto:");
+
+        Console.Write("Nome: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Preço: ");
+        double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Produto produto = new Produto(nome, preco);
+
+        Console.WriteLine();
+
+        Console.WriteLine("Dados do produto: " + produto);
+        Console.WriteLine();
+
+        Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+        int qte = int.Parse(Console.ReadLine());
+        produto.AdicionarProdutos(qte);
+
+        Console.WriteLine();
+        Console.WriteLine("Dados atualizados: " + produto);
+        Console.WriteLine();
+
+        Console.Write("Digite o número de produtos a ser removido do estoque: ");
+        qte = int.Parse(Console.ReadLine());
+        produto.RemoverProdutos(qte);
+
+        Console.WriteLine();
+        Console.WriteLine("Dados atualizados: " + produto);
+    }
+}
