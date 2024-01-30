4d70b9a1f31b9521644e3f563783b8fc4d6d4ac9
Coimbra <adsjkbd@gmail.com>
2024-01-30 08:03:02 -0300

    Encapsulamento

---
+++ Encapsulamento/Encapsulamento/Program.cs
@@ -0,0 +1,15 @@
+﻿using Encapsulamento;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Produto produto = new Produto("TV", 500.00, 10);
+        produto.SetNome("Notebook");
+
+        Console.WriteLine(produto.GetNome());
+        Console.WriteLine(produto.GetQuantidade());
+    }
+}
