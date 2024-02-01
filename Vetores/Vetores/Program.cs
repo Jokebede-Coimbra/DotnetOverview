6eade8393089f3378c21cfc514e6bc0e3b196a44
Coimbra <adsjkbd@gmail.com>
2024-02-01 05:20:51 -0300

    Vetor > tipo struct

---
+++ Vetores/Vetores/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Entre com o tamanho do vetor: ");
+        int n = int.Parse(Console.ReadLine());
+
+        double[] vetor = new double[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            vetor[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        }
+
+        double sum = 0.0;
+
+        for (int i = 0; i < n; i++)
+        {
+            sum += vetor[i];
+        }
+
+        double avg = sum / n;
+
+        Console.WriteLine("Average Height = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+        
+
+    }
+}
