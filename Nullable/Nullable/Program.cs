33ced44b72c29937587257095cd6e92a43aed415
Coimbra <adsjkbd@gmail.com>
2024-02-01 04:51:24 -0300

    Nullable

---
+++ Nullable/Nullable/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nullable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double? x = null;
+            double? y = 20.0;
+
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+
+            if(x.HasValue) 
+            Console.WriteLine(x.Value); // Lança uma execeção > usar o if
+            else
+                Console.WriteLine("X is null");
+
+            if (y.HasValue)
+                Console.WriteLine(y.Value);
+            else
+                Console.WriteLine("Y is null");
+        }
+    }
+}
