db5f10d92042621755ff35f96d9ef9e8e17ff3b5
Coimbra <adsjkbd@gmail.com>
2024-02-01 04:57:32 -0300

    Operador de coalescência nula

---
+++ OperadorCoalescenciaNula/OperadorCoalescenciaNula/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double? x = null;
+        double? y = 20;
+
+        double a = x ?? 5;
+        double b = y ?? 5;
+
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+    }
+}
