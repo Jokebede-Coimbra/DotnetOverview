3243e8792b1e219415ef26a19562b9735bd7f11b
Coimbra <adsjkbd@gmail.com>
2024-02-02 06:49:39 -0300

    Exercicio matrizes

---
+++ ExercicioMatrizes/ExercicioMatrizes/Program.cs
@@ -0,0 +1,53 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Entre com a linha: ");
+        int linha = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Entre com a coluna: ");
+        int coluna = int.Parse(Console.ReadLine());
+
+        int[,] mat = new int[linha, coluna];
+
+        for (int i = 0; i < linha; i++)
+        {
+            string[] values = Console.ReadLine().Split(' ');
+            for (int j = 0; j < coluna; j++)
+            {
+                mat[i, j] = int.Parse(values[j]);
+            }
+        }
+
+        Console.WriteLine("Qual a ocorrência? ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < linha; i++)
+        {
+            for (int j = 0; j < coluna; j++)
+            {
+                if (mat[i, j] == n)
+                {
+                    Console.WriteLine("Position " + i + "," + j + ":");
+                    if (j > 0)
+                    {
+                        Console.WriteLine("Left: " + mat[i, j - 1]);
+                    }
+                    if (i > 0)
+                    {
+                        Console.WriteLine("Up: " + mat[i - 1, j]);
+                    }
+                    if (j < linha - 1)
+                    {
+                        Console.WriteLine("Right: " + mat[i, j + 1]);
+                    }
+                    if (i < coluna - 1)
+                    {
+                        Console.WriteLine("Down: " + mat[i + 1, j]);
+                    }
+                }
+
+            }
+        }
+    }
+}
--- Exercícios/ExercicioLista/ExercicioLista/obj/Release/net8.0/ExercicioLista.AssemblyInfo.cs
+++ Exercícios/ExercicioLista/ExercicioLista/obj/Release/net8.0/ExercicioLista.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExercicioLista")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3a86df5955345e13c0e2f37230dc05475665866")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExercicioLista")]
