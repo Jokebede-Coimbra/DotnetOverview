2a14e1e88240d30412f82b8608386c62d358c8e7
Coimbra <adsjkbd@gmail.com>
2024-02-29 06:16:14 -0300

    Adicionando exercício herança

--- Exercícios/ExercicioColecoes05/ExercicioColecoes05/obj/Release/net8.0/ExercicioColecoes05.AssemblyInfo.cs
+++ Exercícios/ExercicioColecoes05/ExercicioColecoes05/obj/Release/net8.0/ExercicioColecoes05.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExercicioColecoes05")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c34158af8e95543ceae718dcd1cead26b4590ead")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7e7fa3ccc62b3a5853b379dbf5c7c063bbe0da1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExercicioColecoes05")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExercicioColecoes05")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Exercícios/ExercicioHerancaII/ExercicioHeranca/Program.cs
@@ -0,0 +1,58 @@
+﻿using ExercicioHeranca;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("\nConta Corrente: Número: 101 Titular: Jayanne");
+        Conta conta = new();
+        conta.Numero = 101;
+        conta.Titular = "Jayanne";
+
+        Console.WriteLine("Despositando o R$1.000,00");
+        conta.Depositar(1000);
+        Console.WriteLine($"Saldo da conta: {conta.Saldo.ToString("c")}");
+        Console.WriteLine("Sacando R$100,00");
+        conta.Sacar(100);
+        Console.WriteLine($"Saldo da conta: " + conta.Saldo.ToString("c"));
+        Console.WriteLine("\nSacando R$ 2.000,00");
+        conta.Sacar(2000);
+        Console.WriteLine($"Saldo da conta: {conta.Saldo.ToString("c")}");
+
+        Console.ReadKey();
+
+        Console.WriteLine("\nConta Poupança: Numero: 102 Titular: Samarah");
+        ContaPoupanca contaPoupanca = new();
+        contaPoupanca.Numero = 102;
+        contaPoupanca.Titular = "Samarah";
+        Console.WriteLine("Despositando o R$1000,00");
+        contaPoupanca.Depositar(1000);
+        Console.WriteLine($"Saldo da Conta Poupanca: {contaPoupanca.Saldo.ToString("c")}");
+        Console.WriteLine("Sacando R$100,00");
+        contaPoupanca.Sacar(100);
+        Console.WriteLine($"Saldo da Conta Poupanca: {contaPoupanca.Saldo.ToString("c")}");
+        Console.WriteLine("\nSacando R$ 2.000,00");
+        contaPoupanca.Sacar(2000);
+        Console.WriteLine($"Saldo da conta poupança: " + contaPoupanca.Saldo.ToString("c"));
+
+
+        Console.ReadKey();
+
+        Console.WriteLine("\nConta Poupança: Numero: 103 Titular: Jacson");
+        ContaInvestimento contaInvestimento = new();
+        contaInvestimento.Numero = 103;
+        contaInvestimento.Titular = "Jacson";
+        Console.WriteLine("Despositando o R$1000,00");
+        contaInvestimento.Depositar(1000);
+        Console.WriteLine($"Saldo da Conta Poupanca: {contaInvestimento.Saldo.ToString("c")}");
+        Console.WriteLine("Sacando R$100,00");
+        contaInvestimento.Sacar(100);
+        Console.WriteLine($"Saldo da Conta Poupanca: {contaInvestimento.Saldo.ToString("c")}");
+
+        Console.WriteLine("\nSacando R$ 2.000,00");
+        contaInvestimento.Sacar(2000);
+        Console.WriteLine($"Saldo da conta investimento: " + contaInvestimento.Saldo.ToString("c"));
+
+        Console.ReadKey();
+    }
+}
