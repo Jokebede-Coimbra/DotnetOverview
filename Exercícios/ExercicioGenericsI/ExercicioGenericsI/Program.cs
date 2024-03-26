325ad1351bae68a0d0d053f5168cf07a40787ffe
Coimbra <adsjkbd@gmail.com>
2024-03-26 07:47:04 -0300

    Exercicio Generics

---
+++ Exercícios/ExercicioGenericsI/ExercicioGenericsI/Program.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ClasseGenerica<int> objInt = new ClasseGenerica<int>();
+        objInt.Adicionar(101);
+        objInt.Adicionar(102);
+        objInt.Adicionar(103);
+        objInt.Adicionar(104);
+        objInt.Adicionar(105);
+
+        Console.WriteLine("Classe Genérica<T> de objeto int\n");
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(objInt[i]);
+        }
+    }
+
+    public class ClasseGenerica<T>
+    {
+        T[] obj = new T[5];
+        int contador = 0;
+
+        public void Adicionar(T item)
+        {
+            if (contador < 5)
+            {
+                obj[contador] = item;
+            }
+            contador++;
+        }
+
+        // indexador
+        public T this[int index]
+        {
+            get { return obj[index]; }
+            set { obj[index] = value; }
+        }
+
+    }
+}
--- Struct/Struct/obj/Debug/net8.0/Struct.AssemblyInfo.cs
+++ Struct/Struct/obj/Debug/net8.0/Struct.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Struct")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a0503d926b76403b64d7c9991fbedda3cdb9853")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3fb0012843ddc5fbbc256b393383847e3265178")]
 [assembly: System.Reflection.AssemblyProductAttribute("Struct")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Struct")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
